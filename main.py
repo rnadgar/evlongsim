4a7de36d9b472a9024662fffd7b23bc92e075278
rnadgar <jakobmadgar@gmail.com>
2022-03-09 08:09:35 -0800

    Updated

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
+import streamlit as st
+from tqdm import tqdm
 
 # Scope:
 # 4 Wheel drive model of RC Car. This will be used to spec a battery and motor for FRC. 
-# Will not consider any temperature, time, or other nonlinear effects besides tire nonlinearity
+# Will try to incorporate as many nonlinearities as possible with the motor, battery, and controller
 # This script will simulate a straightline acceleration at the limits of the battery, tires, motor, and aerodynamics
 # Drag and Lift is considered as estimations. Bluff body drag will be considered
 
-# Want to incorporate into streamlit
+# TODO: Add preconfigured options to the classes to make testing easier. Along with custom option
+# TODO: Learn how to make and import modules
 
+# TODO: Better Charactize the Motor. Make Motor Efficiency map. Need to make other script for this
+# TODO: Better Charactize the battery. Include temperature and other effects from manufacturer
+# TODO: Further down the line but better understand the controller and integrate it into this script
 
 class Vehicle:
     ''' 
@@ -65,19 +71,20 @@ class Tire:
     | r is the tire radius (meters) (Should be effective radius if known)
     | J is wheel inertia (kg-m^2)
     | a,b,c, and d are the pacejka coefficents
+    | fudge is the decrease in friction force from the data at calspan to the road
     '''
-    def __init__(self, r, J, a, b, c, d):
+    def __init__(self, r, J, a, b, c, d, fudge):
         self.r = r
         self.a = a
         self.b = b
         self.c = c
         self.d = d
         self.J = J
+        self.fudge = fudge
     
-    def lonFriction(self,slip,normalForce):
-        Fx = normalForce * self.d * np.sin(self.c * np.arctan(self.b*slip - self.a * (self.b*slip - np.arctan(self.b*slip))))
-        u = Fx/normalForce
-        return u
+    def lonForce(self,slip,normalForce):
+        Fx = -normalForce * self.d * np.sin(self.c * np.arctan(self.b*slip - self.a * (self.b*slip - np.arctan(self.b*slip))))
+        return Fx
 
 class Sim:
     ''' 
@@ -100,10 +107,10 @@ class Sim:
         self._current_x_ddot = 0
         self._current_time = 0
         self._current_amps = [0, 0, 0, 0]
-        self._inital_Fz =  [Vehicle.m * Vehicle.b/Vehicle.l / 2, Vehicle.m * Vehicle.b/Vehicle.l / 2, 
-                            Vehicle.m * Vehicle.a/Vehicle.l / 2, Vehicle.m * Vehicle.a/Vehicle.l / 2]
-        self._current_Fz = [Vehicle.m * Vehicle.b/Vehicle.l / 2, Vehicle.m * Vehicle.b/Vehicle.l / 2, 
-                            Vehicle.m * Vehicle.a/Vehicle.l / 2, Vehicle.m * Vehicle.a/Vehicle.l / 2]
+        self._inital_Fz =  [-9.81 * Vehicle.m * Vehicle.b/Vehicle.l / 2, -9.81 * Vehicle.m * Vehicle.b/Vehicle.l / 2, 
+                            -9.81 * Vehicle.m * Vehicle.a/Vehicle.l / 2, -9.81 * Vehicle.m * Vehicle.a/Vehicle.l / 2]
+        self._current_Fz = [-9.81 * Vehicle.m * Vehicle.b/Vehicle.l / 2, -9.81 * Vehicle.m * Vehicle.b/Vehicle.l / 2, 
+                            -9.81 * Vehicle.m * Vehicle.a/Vehicle.l / 2, -9.81 * Vehicle.m * Vehicle.a/Vehicle.l / 2]
         self._current_P = [0, 0, 0, 0]
         self._current_slip = [0, 0, 0, 0]
         self._current_w = [0, 0, 0, 0]
@@ -134,7 +141,7 @@ class Sim:
 
     def w_fun(self, w, T, axle):
         # This function solves for the rotational velocity of the wheel given a driveline torque. This is used when the vehicle is amperage limited
-        return self.Tire.J * (w - self._current_w[axle]) + self.Tire.r * self.Tire.lonFriction((1-(self._current_x_dot/(self.Tire.r * w))),self._current_Fz[axle]) * self._current_Fz[axle] - T
+        return self.Tire.J * (w - self._current_w[axle]) + self.Tire.r * self.Tire.lonForce((1-(self._current_x_dot/(self.Tire.r * w))),self._current_Fz[axle]) * self.Tire.fudge - T
 
     def accel_FrictionLimited(self):
 
@@ -153,10 +160,10 @@ class Sim:
         # Finding Peak Force Avaliable
         for n in range(100):
             slip_it = n/100
-            P_rf_list.append(self.Tire.lonFriction(slip_it,self._current_Fz[0]) * self._current_Fz[0])
-            P_lf_list.append(self.Tire.lonFriction(slip_it,self._current_Fz[1]) * self._current_Fz[1])
-            P_lr_list.append(self.Tire.lonFriction(slip_it,self._current_Fz[2]) * self._current_Fz[2])
-            P_rr_list.append(self.Tire.lonFriction(slip_it,self._current_Fz[3]) * self._current_Fz[3])
+            P_rf_list.append(self.Tire.lonForce(slip_it,self._current_Fz[0]) * self.Tire.fudge)
+            P_lf_list.append(self.Tire.lonForce(slip_it,self._current_Fz[1]) * self.Tire.fudge)
+            P_lr_list.append(self.Tire.lonForce(slip_it,self._current_Fz[2]) * self.Tire.fudge)
+            P_rr_list.append(self.Tire.lonForce(slip_it,self._current_Fz[3]) * self.Tire.fudge)
 
         # Finding Slip at Peak Force
         P[0] = max(P_rf_list)
@@ -183,20 +190,23 @@ class Sim:
         for n in range(len(P)):
             amps[n] = T[n] / self.Motor.Kt * (1 / self.Motor.k)
 
-        # print('friction' + str(w_r))
+        # print('Torque Tire : ' + str(T[2]) + ' Slip Tire : ' + str(slip[2])) FIXME: Remove
+        # print('WS Tire : ' + str(w[2])) FIXME: Remove
 
         # Setting Amperage Limit and back solving for other variables
         for n in range(len(self._current_Fz)):
             if amps[n] > self.Battery.Burst:
                 amps[n] = self.Battery.Burst
                 T[n] = amps[n] * self.Motor.Kt * (1 / self.Motor.k)
-                w[n] = fsolve(self.w_fun,1,args=(T[n], n))
-                slip[n] = 1 - self._current_x_dot / (self.Tire.r * w[n])
-                P[n] = self.Tire.lonFriction(slip[n],self._current_Fz[n]) * self._current_Fz[n]
+                guess = w[n] * 0.8
+                w[n] = fsolve(self.w_fun,guess,args=(T[n], n))
+                slip[n] = (self.Tire.r * w[n] - self._current_x_dot) / (self.Tire.r * w[n])
+                P[n] = self.Tire.lonForce(slip[n],self._current_Fz[n]) * self.Tire.fudge
+
+        # print('Torque Amp : ' + str(T[2]) + ' Slip Amp : ' + str(slip[2])) FIXME: Remove
 
         # Air Resistance
         F_aero = (self.Vehicle.cd * 1.225 * self._current_x_dot**2 * self.Vehicle.A) / 2
-        print(F_aero)
 
         # Longitudinal Acceleration
         x_ddot = (sum(P) - F_aero) / self.Vehicle.m 
@@ -208,7 +218,7 @@ class Sim:
         x = self._current_x + x_dot * self.dt
 
         # Weight Transfer
-        wtfr = self.Vehicle.h / self.Vehicle.l * self.Vehicle.m * x_ddot / 9.81
+        wtfr = self.Vehicle.h / self.Vehicle.l * self.Vehicle.m * x_ddot / -9.81
 
         # New Normal Forces
         for n in range(len(P)):
@@ -221,7 +231,7 @@ class Sim:
         return x, x_dot, x_ddot, time, amps, Fz, P, slip, w
 
     def __call__(self):
-        for dt in range(int(self.runtime/self.dt)):
+        for dt in tqdm(range(int(self.runtime/self.dt)),'Running Sim : '):
             current = self.accel_FrictionLimited()
             self.update_values(*current)
             output_data =  [self._current_time, 
@@ -236,24 +246,54 @@ class Sim:
             output = pd.DataFrame(np.array(output_data, dtype=object).reshape(-1,len(output_data)),columns = self.column_names)
             self.output = pd.concat([self.output,output], ignore_index=True)
 
+# Defining Vehicle
 frc_vehicle = Vehicle(0.126,0.126,5,0.032,0.75,0.0418)
 frc_motor = Motor(2000,0.8)
-frc_tire = Tire(0.032,0.00001667,1.0301,16.6675,0.05343,65.1759)
+frc_tire = Tire(0.032,0.00001667,1.0301,16.6675,0.05343,65.1759,0.5)
 frc_battery = Battery(10,5,3.6,8)
 
-sim = Sim(frc_vehicle,frc_motor,frc_tire,frc_battery,0.01,20)
+# Creating Sim Object
+sim = Sim(frc_vehicle,frc_motor,frc_tire,frc_battery,0.01,10)
 
+# Running Sim
 sim()
 
+# Getting Output
 data = sim.output
 
-fig, axs = plt.subplots(3, 3)
+fig = plt.figure(figsize=plt.figaspect(4))
+gs = fig.add_gridspec(4, hspace=0.33)
+axs = gs.subplots()
+
+axs[0].plot(data['Right Front Wheel Speed (rad/s)'], label = 'Right Front Wheel Speed (rad/s)')
+axs[0].plot(data['Right Rear Wheel Speed (rad/s)'], label = 'Right Rear Wheel Speed (rad/s)')
+axs[0].legend()
+
+axs[1].plot(data['Right Front Slip Ratio (-)'], label = 'Right Front Slip Ratio (-)')
+axs[1].plot(data['Right Rear Slip Ratio (-)'], label = 'Right Rear Slip Ratio (-)')
+axs[1].legend()
+
+# axs[2].plot(data['FZ Right Front (N)'], label = 'FZ Right Front (N)')
+# axs[2].plot(data['FZ Right Rear (N)'], label = 'FZ Right Rear (N)')
+axs[2].plot(data['FX Right Front (N)'], label = 'FX Right Front (N)')
+axs[2].plot(data['FX Right Rear (N)'], label = 'FX Right Rear (N)')
+axs[2].legend()
+axs[2].set_ylim([0, 50])
+
+axs[3].plot(data['x_dot (m/s)'], label = 'x_dot (m/s)')
+axs[3].plot(data['x_ddot (m/s/s)'], label = 'x_ddot (m/s/s)')
+axs[3].legend()
 
 
-plt.figure()
-# plt.plot(data['Front Wheel Speed (rad/s)'], label = 'Front Wheel Speed (rad/s)')
-plt.plot(data['Right Front amps (A)'], label = 'Right Front amps (A)')
-plt.plot(data['Right Rear amps (A)'], label = 'Right Rear amps (A)')
-# plt.plot(data['x_dot (m/s)'], label = 'x_dot (m/s)')
-plt.legend()
-plt.show()
+st.pyplot(fig)
+
+# newslip = np.linspace(-1,1,200)
+
+# Fz = np.repeat(-40,len(newslip))
+
+# fx = frc_tire.lonForce(newslip,Fz) * frc_tire.fudge
+
+# fig = plt.figure()
+# plt.plot(newslip,fx)
+
+# st.pyplot(fig)
